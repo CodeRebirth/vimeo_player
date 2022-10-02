27553e87572d3b7db4179d93af3cdaad35c7825f
TraderLeo <sixpathssenin@gmail.com>
2022-10-02 20:16:43 +0530

    added new commit for new method of accessing video info

--- lib/src/quality_links.dart
+++ lib/src/quality_links.dart
@@ -15,14 +15,12 @@ class QualityLinks {
   }
 
   Future<SplayTreeMap?> getQualitiesAsync() async {
+    print(videoId);
     try {
-      var response = await http
-          .get(Uri.parse('https://player.vimeo.com/video/' + videoId + '/config'));
-      var jsonData =
-          jsonDecode(response.body)['request']['files']['progressive'];
-      SplayTreeMap videoList = SplayTreeMap.fromIterable(jsonData,
-          key: (item) => "${item['quality']} ${item['fps']}",
-          value: (item) => item['url']);
+      // var response = await http.get(Uri.parse('https://player.vimeo.com/video/' + videoId + '/config'));
+      var response = await http.get(Uri.parse('https://api.vimeo.com/videos/' + videoId), headers: {"Authorization": "Bearer 0e735c8e74e480ef3d2d6d649e1630f9"});
+      var jsonData = jsonDecode(response.body)['files'];
+      SplayTreeMap videoList = SplayTreeMap.fromIterable(jsonData, key: (item) => "${item['public_name']} ${item['fps']}", value: (item) => item['link']);
       return videoList;
     } catch (error) {
       print('=====> REQUEST ERROR: $error');
